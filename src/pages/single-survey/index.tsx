3f34318382bf0b06bda34d416ab425baeb1093f9
mo-renike <herroyalpianist@gmail.com>
2023-08-04 17:40:44 +0100

    code cleanup

--- src/pages/single-survey/index.tsx
+++ src/pages/single-survey/index.tsx
@@ -8,7 +8,8 @@ const SingleSurvey = () => {
   const [currentPage, setCurrentPage] = React.useState(1);
   const { id } = useParams();
   const { surveys } = React.useContext(AppContext);
-
+  // State to store user responses
+  const [userResponses, setUserResponses] = React.useState<Question[]>([]);
   const currentSurvey = surveys.find((item) => item.id === Number(id));
 
   if (!currentSurvey) {
@@ -35,34 +36,55 @@ const SingleSurvey = () => {
   // Calculate the current question number based on the currentPage and QUESTIONS_PER_PAGE
   const currentQuestionNumber = (currentPage - 1) * QUESTIONS_PER_PAGE;
 
+  // Function to handle user responses
+  const handleResponseChange = (questionId: number, response: any) => {
+    setUserResponses((prevResponses) => ({
+      ...prevResponses,
+      [questionId]: response,
+    }));
+  };
+
+  // Function to handle form submission
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    console.log("User Responses:", userResponses);
+  };
+
   return (
     <div>
       <h1>
         Survey {currentSurvey.id} <small>Number of steps: {totalPages}</small>{" "}
       </h1>
-      <div>
-        <h3>Questions:</h3>
-        {displayedQuestions.map((question: Question, index: number) => (
-          <QuestionComponent
-            key={question.id}
-            question={question}
-            questionNumber={currentQuestionNumber + index + 1}
-          />
-        ))}
-        {totalQuestions > QUESTIONS_PER_PAGE && (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <h3>Questions:</h3>
+          {displayedQuestions.map((question) => (
+            <QuestionComponent
+              key={question.id}
+              question={question}
+              questionNumber={currentQuestionNumber + 1}
+              onChange={(response) =>
+                handleResponseChange(question.id, response)
+              }
+              userResponse={userResponses[question.id]}
+            />
+          ))}
+          {/* {totalQuestions > QUESTIONS_PER_PAGE && ( */}
           <div>
             <button onClick={handlePrevPage} disabled={currentPage === 1}>
               Previous Step
             </button>
             <button
+              type="submit"
               onClick={handleNextPage}
               disabled={currentPage === totalPages}
             >
-              Next Step
+              {currentPage === totalPages ? "Submit" : "Next Step"}
             </button>
           </div>
-        )}
-      </div>
+          {/* )} */}
+        </div>
+      </form>
       <h3>
         Step {currentPage} of {totalPages}
       </h3>
@@ -75,60 +97,72 @@ const SingleSurvey = () => {
 const QuestionComponent = ({
   question,
   questionNumber,
+  onChange,
+  userResponse,
 }: {
   question: Question;
   questionNumber: number;
+  onChange: (response: any) => void;
+  userResponse: any;
 }) => {
   const { type, text, options } = question;
 
-  if (type === "radio") {
-    return (
-      <div>
-        <h4>
-          Question {questionNumber}: {text}
-        </h4>
-        {options && (
-          <ul>
-            {options.map((option, index) => (
-              <li key={index}>
-                <input type="radio" value={option} name={text} />
-                {option}
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    );
-  } else if (type === "text") {
-    return (
-      <div>
-        <h4>
-          Question {questionNumber}: {text}
-        </h4>
-        <input type="text" />
-      </div>
-    );
-  } else if (type === "checkbox") {
-    return (
-      <div>
-        <h4>
-          Question {questionNumber}: {text}
-        </h4>
-        {options && (
-          <ul>
-            {options.map((option, index) => (
-              <li key={index}>
-                <input type="checkbox" value={option} name={text} />
-                {option}
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    );
-  }
+  const handleChange = (event: {
+    target: { value: any; checked: any; type: any };
+  }) => {
+    const { value, checked, type } = event.target;
+    const response = type === "checkbox" ? checked : value;
+    onChange(response);
+  };
 
-  return null;
+  const renderOptions = () => {
+    return (
+      <ul>
+        {options ? (
+          options.map((option, index) => (
+            <li key={index}>
+              <input
+                type={type}
+                value={option}
+                name={text}
+                onChange={handleChange}
+                checked={
+                  userResponse === option ||
+                  (Array.isArray(userResponse) && userResponse.includes(option))
+                }
+              />
+              {option}
+            </li>
+          ))
+        ) : (
+          <input
+            type={type}
+            name={text}
+            onChange={handleChange}
+            value={userResponse || ""}
+          />
+        )}
+      </ul>
+    );
+  };
+
+  const renderQuestion = () => (
+    <div>
+      <h4>
+        Question {questionNumber}: {text}
+      </h4>
+      {renderOptions()}
+    </div>
+  );
+
+  switch (type) {
+    case "radio":
+    case "checkbox":
+    case "text":
+      return renderQuestion();
+    default:
+      return null;
+  }
 };
 
 export default SingleSurvey;
